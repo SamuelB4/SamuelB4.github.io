7a2de5438122e043d523d2ad8df02df42df00a42
SamuelB4 <128752342+SamuelB4@users.noreply.github.com>
2024-09-28 21:43:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function correctQ1() {
+    document.getElementById("quesOne").innerHTML="Correct!";
+}
+
+function incorrectQ1() {
+    document.getElementById("quesOne").innerHTML="Incorrect!"
+}
+
+function correctQ2() {
+    document.getElementById("quesTwo").innerHTML="Correct!";
+}
+
+function incorrectQ2() {
+    document.getElementById("quesTwo").innerHTML="Incorrect!"
+}
+
+function correctQ3() {
+    document.getElementById("questThree").innerHTML="Correct!";
+}
+
+function incorrectQ3() {
+    document.getElementById("quesThree").innerHTML="Incorrect!"
+}
